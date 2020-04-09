0da3cd2a391a6cfb050903775320083142dd6da1
SuvorovNikita <nikit.suvorv@gmail.com>
2020-04-09 14:09:04 +0300

    update

--- src/component/Skills/CardItem/CardItem.jsx
+++ src/component/Skills/CardItem/CardItem.jsx
@@ -3,12 +3,12 @@ import styles from "./CardItem.module.css";
 
 const CardItem = () => {
     return (
-        <carditem className={styles.cardItem}>
+        <div className={styles.cardItem}>
             <icon className={styles.icon}>Icon
                 <img src="" alt=""/></icon>
             <h3 className={styles.name}>React</h3>
-            <description className={styles.description}> Подробное описание</description>
-        </carditem>
+            <div className={styles.description}> Подробное описание</div>
+        </div>
     )
 };
 
