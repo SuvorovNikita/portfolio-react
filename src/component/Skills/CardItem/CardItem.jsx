bf8c8f4409ad55473ef8c286bc1ce3c4e0e6214e
SuvorovNikita <nikit.suvorv@gmail.com>
2020-06-03 12:30:54 +0300

    section skills

--- src/component/Skills/CardItem/CardItem.jsx
+++ src/component/Skills/CardItem/CardItem.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import styles from "./CardItem.module.scss";
 
-const CardItem = () => {
+
+const CardItem = (props) => {
     return (
         <div className={styles.cardItem}>
-            <icon className={styles.icon}>Icon
-                <img src="" alt=""/></icon>
-            <h3 className={styles.name}>React</h3>
-            <div className={styles.description}> Подробное описание</div>
+            <div className={styles.iconBox}>{props.FontAwesomeIcon}</div>
+            <h3 className={styles.name}>{props.name}</h3>
+            <div className={styles.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua.
+            </div>
         </div>
     )
 };
--- src/component/Skills/Skills.jsx
+++ src/component/Skills/Skills.jsx
@@ -1,19 +1,25 @@
 import React from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faHtml5, faReact} from "@fortawesome/free-brands-svg-icons";
 
 import CardItem from "./CardItem/CardItem";
 import Title from "../../common/Title/title";
 
 import styles from "./Skills.module.scss";
 
+
 const Skills = () => {
     return (
         <div className={styles.skills}>
             <div className={styles.container}>
-              <Title text='Скилуха'/>
+                <Title text='Skills'/>
                 <div className={styles.card}>
-                    <CardItem/>
-                    <CardItem/>
-                    <CardItem/>
+                    <CardItem name='React'
+                              FontAwesomeIcon={<FontAwesomeIcon className={styles.reactIcon} icon={faReact}/>}/>
+                    <CardItem name='HTML5'
+                              FontAwesomeIcon={<FontAwesomeIcon className={styles.reactIcon} icon={faHtml5}/>}/>
+                    <CardItem name='Redux'
+                              FontAwesomeIcon={<FontAwesomeIcon className={styles.reactIcon} icon={faReact}/>}/>
                 </div>
             </div>
         </div>
