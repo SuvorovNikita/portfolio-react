bcbb0e21ad33ac82c877cc21c179baa8ac581b6e
SuvorovNikita <nikit.suvorv@gmail.com>
2020-08-31 17:16:23 +0300

    mobile menu

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import styles from "./Header.module.scss";
 import Nav from "./nav/Nav";
+import Mobilemenu from "./mobile-menu/Mobilemenu";
 
 const Header = () => {
     return (
         <div className={styles.header}>
             <div className={styles.container}>
                <Nav/>
+               <Mobilemenu/>
             </div>
         </div>
     )
--- src/component/Header/mobile-menu/Mobilemenu.jsx
+++ src/component/Header/mobile-menu/Mobilemenu.jsx
@@ -5,15 +5,12 @@ import {Link} from "react-scroll";
 import styles from "./Mobilemenu.module.scss";
 
 const Mobilemenu = () => {
-
     return (
-            <nav className={styles.menuMobile}>
-                <div className={mobileMenuActive}>
-                    <Link className={styles.navlink} smooth={true} offset={1} to="skills">Skills</Link>
-                    <Link className={styles.navlink} smooth={true} offset={1} to="work">Project</Link>
-                    <Link className={styles.navlink} smooth={true} offset={1} to="contact">Contacts</Link>
-                </div>
-            </nav>
+        <nav className={styles.mobileWrapper}>
+            <Link className={styles.navlink} smooth={true} offset={1} to="skills">Skills</Link>
+            <Link className={styles.navlink} smooth={true} offset={1} to="work">Project</Link>
+            <Link className={styles.navlink} smooth={true} offset={1} to="contact">Contacts</Link>
+        </nav>
     )
 };
 
