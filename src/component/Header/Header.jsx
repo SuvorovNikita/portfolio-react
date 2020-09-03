3a1407869abea96ca5e909ec8c341c2d97a38610
Никита <nikit.suvorov@gmail.com>
2020-09-03 20:16:58 +0300

    'end'

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -5,7 +5,7 @@ import Mobilemenu from "./mobile-menu/Mobilemenu";
 
 const Header = () => {
     return (
-        <div className={styles.header} id='main'>
+        <div className={styles.header}  >
             <div className={styles.container}>
                 <Nav/>
                 <Mobilemenu/>
